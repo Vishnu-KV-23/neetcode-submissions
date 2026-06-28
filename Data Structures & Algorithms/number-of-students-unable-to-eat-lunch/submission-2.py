b8944322f1b156e85a5dda60f2825f96076fba34
Vishnu K V <wishnukv2349@gmail.com>
2026-06-28 23:18:50 +0530

    Add: number-of-students-unable-to-eat-lunch - submission-2

---
+++ Data Structures & Algorithms/number-of-students-unable-to-eat-lunch/submission-2.py
@@ -0,0 +1,18 @@
+from collections import Counter
+class Solution:
+    def countStudents(self, students: List[int], sandwiches: List[int]) -> int:
+        wants={}
+        rem=len(students)
+        for i in students:
+            if i in wants:
+                wants[i]+=1
+            else:
+                wants[i]=1
+        for i in sandwiches:
+            if wants[i] > 0:
+                wants[i]-=1
+                rem-=1
+            else:
+                return rem
+        
+        
